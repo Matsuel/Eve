75f2fcf6a4fbe5c3d722e167c4d8283c5af4dc13
Matsuel <matsuel0308@gmail.com>
2024-12-03 10:56:54 +0100

    edit chill guy title

--- src/commands/fun/chill.ts
+++ src/commands/fun/chill.ts
@@ -7,8 +7,7 @@ export const data: SlashCommandBuilder = new SlashCommandBuilder()
 
 export async function execute(interaction: CommandInteraction): Promise<void> {
     const embed = new EmbedBuilder()
-        .setTitle("Chill")
-        .setDescription("Gif du mec chill")
+        .setTitle("Juste un mec chill")
         .setImage("https://tenor.com/view/chill-guy-my-new-character-gif-2777893510283028272")
 
     await interaction.reply({ embeds: [embed], ephemeral: false })
